826c70f54fd05cb32e856a5c2af974e6835108ae
Eoghan Martin <eoghan2014@gmail.com>
2020-02-16 21:29:49 +0000

    rose

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -6,12 +6,12 @@ public class Loops extends PApplet
 {	
 	public void settings()
 	{
-		size(1000, 1000);
+		size(400, 400);
 	}
 
 	public void setup() 
 	{
-		
+		colorMode(HSB,130);
 	}
 
 	
@@ -31,31 +31,64 @@ public class Loops extends PApplet
 		// 	i++;
 		// }
 		// stroke(0);
-		// for(int x=0;x<1000;x += 10)
+		// for(int x=0;x<440;x += 10)
 		// {
-		// 	line(x,0,500,500);
+		// 	line(x,0,220,220);
 		// }
-		// for(int y=0;y<1000;y += 10)
+		// for(int y=0;y<440;y += 10)
 		// {
-		// 	line(1000,y,500,500);
+		// 	line(440,y,220,220);
 		// }
-		// for(int x2=1000;x2>0;x2 -= 10)
+		// for(int x2=440;x2>0;x2 -= 10)
 		// {
-		// 	line(x2,1000,500,500);
+		// 	line(x2,440,220,220);
 		// }
-		// for(int y2=1000;y2>0;y2 -= 10)
+		// for(int y2=440;y2>0;y2 -= 10)
 		// {
-		// 	line(0,y2,500,500);
+		// 	line(0,y2,220,220);
 		// }
+
+
+
+		noStroke();
 			int y;
 			int x;
-			int h=40;
-		for(x=20;x<1000;x+=40)
+			int w=40;
+			float h=0;
+		for(x=20;x<400;x+=40)
 		{
-			for(y=20;y<1000;y+=40)
+			
+			for(y=20;y<400;y+=40)
 			{
-				ellipse(x,y,h,h);
+				// if(h==100)
+				// {
+				// 	h=0;
+				// }
+				fill(h,130,130);
+				ellipse(x,y,w,w);
+				h+=5;
 			}
+			h-=45;
 		}
+		// int m=0;
+		// for(x=0;x<440;x+=40)
+		// {
+		// 	for(y=0;y<440;y+=40)
+		// 	{
+		// 		if(m==0)
+		// 		{
+		// 			fill(0,0,153);
+		// 			rect(x, y, h, h);
+		// 			m=1;
+		// 		}
+		// 		else if(m==1)
+		// 		{
+		// 			fill(0,102,204);
+		// 			rect(x, y, h, h);
+		// 			m=0;
+		// 		}
+		// 	}
+		// }
+		
 	}
 }
